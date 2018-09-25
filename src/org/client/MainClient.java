4155fd9e29e21361d71786fc9f7b91f4dd0d0bf8
Jordan Morin <16jordan65@gmail.com>
2018-09-25 14:51:47 -0400

    server connection

--- src/org/client/MainClient.java
+++ src/org/client/MainClient.java
@@ -1,5 +1,68 @@
 package org.client;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class MainClient {
+	public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException {
+		
+		Socket clientSocket = null;
+		try {
+			clientSocket = new Socket("127.0.0.1", 5000);
+			ObjectOutputStream objectOutput = new ObjectOutputStream(clientSocket.getOutputStream());
+			List<String> linesToSend = readFile("text.txt");
+			objectOutput.writeObject(linesToSend);
+			objectOutput.flush();
+			ObjectInputStream obj = new ObjectInputStream(clientSocket.getInputStream());
+			@SuppressWarnings("unchecked")
+			Stack<String> receivedStack = (Stack<String>) obj.readObject();
+			writeToFile(receivedStack, "FichierInversee.txt");
+		} finally {
+			clientSocket.close();
+		}
+	}
+	
+	// Fonction permettant de lire un fichier et de stocker son contenu dans une liste.
+	private static List<String> readFile(String nomFichier) throws IOException {
+		List<String> listOfLines = new ArrayList<String>();
+		String line = null;
+		FileReader fileReader = null;
+		BufferedReader bufferedReader = null;
+		try {
+			fileReader = new FileReader(nomFichier);
 
+			bufferedReader = new BufferedReader(fileReader);
+
+			while ((line = bufferedReader.readLine()) != null) {
+				listOfLines.add(line);
+			}
+		} finally {
+			fileReader.close();
+			bufferedReader.close();
+		}
+		return listOfLines;
+	}
+
+	// Fonction permettant d'écrire dans un fichier les données contenues dans la
+	// stack reçu du serveur.
+	private static void writeToFile(Stack<String> myStack, String nomFichier) throws IOException {
+		BufferedWriter out = null;
+		try {
+			out = new BufferedWriter(new FileWriter(nomFichier));
+			while (!myStack.isEmpty()) {
+				out.write(myStack.pop() + "\n");
+			}
+		} finally {
+			out.close();
+		}
+	}
 }
--- src/org/server/MainServer.java
+++ src/org/server/MainServer.java
@@ -1,5 +1,41 @@
 package org.server;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.Stack;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
+import java.io.InputStream;
+import java.util.Scanner;
 
 public class MainServer {
-
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		while (true) {
+			ServerSocket serverSocket = null;
+			Scanner scanner = null;
+						
+			try {
+				scanner = new Scanner( System.in );
+				System.out.print( "Entrez l'addresse IP du serveur: ");
+				String serverAddress = scanner.nextLine();
+				System.out.print( "Entrez le port d'écoute: ");
+				int port = scanner.nextInt();
+				
+				InetAddress IPAddress = InetAddress.getByName(serverAddress);
+				serverSocket = new ServerSocket();
+				serverSocket.setReuseAddress(true);
+				serverSocket.bind(new InetSocketAddress(IPAddress, port));
+				
+			} finally {
+				serverSocket.close();
+				scanner.close();			
+			}
+		}
+	}
 }
