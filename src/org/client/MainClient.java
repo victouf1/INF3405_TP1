3ed0f56cafafa745f98afb0f279e7d2c96705d85
Victouf <victouf50@gmail.com>
2018-10-22 20:16:51 -0400

    communications avec le client pour les commandes simples

--- src/org/client/MainClient.java
+++ src/org/client/MainClient.java
@@ -1,11 +1,15 @@
 package org.client;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
@@ -14,21 +18,25 @@ import javax.swing.JTextField;
 
 public class MainClient {
 
-	private DataInputStream in;
-	private DataOutputStream out;
+	private ObjectInputStream in;
+	private PrintWriter out;
 	private JFrame frame = new JFrame("Capitalize Client");
 	private JTextField dataField = new JTextField(40);
 	private JTextArea messageArea = new JTextArea(8, 60);
-	
+
 	public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException {
 
 		MainClient client = new MainClient();
 		client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		client.frame.pack();
 		client.frame.setVisible(true);
-		client.connectToServer();
+		try {
+			client.connectToServer();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	public MainClient() {
 
 		// Layout GUI
@@ -45,34 +53,44 @@ public class MainClient {
 			 * closes all sockets, streams and windows.
 			 */
 			public void actionPerformed(ActionEvent e) {
-				//envoie du fichier
-				// TODO reception du fichier
-//				Util.recieveFile(dataField.getText(), in);
-				messageArea.append("succeeeeeed \n");
+				messageArea.append(dataField.getText());
+				out.println(dataField.getText());
+				out.flush();
+				List<String> response;
+				try {
+					response = (List<String>) in.readObject();
+					if (response == null || response.equals("")) {
+						System.exit(0);
+					}
+				} catch (IOException | ClassNotFoundException ex) {
+					response = new ArrayList<>();
+					response.add("Error: " + ex);
+				}
+				messageArea.append(response + "\n");
 				dataField.selectAll();
 			}
 		});
 	}
-	
+
 	@SuppressWarnings("resource")
 	public void connectToServer() throws IOException {
 
 		// Get the server address from a dialog box.
 		String serverAddress = GuiUtil.getIPAdress();
 		int port = GuiUtil.getPort();
-
+		// CONNECTEXCEPTION
 		Socket socket;
 		socket = new Socket(serverAddress, port);
 
 		System.out.format("The capitalization server is running on %s:%d%n", serverAddress, port);
 
-		in = new DataInputStream(socket.getInputStream());
-		out = new DataOutputStream(socket.getOutputStream());
+		in = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+		out = new PrintWriter(socket.getOutputStream());
 
 		// Consume the initial welcoming messages from the server
 		for (int i = 0; i < 3; i++) {
 			messageArea.append(i + "\n");
 		}
 	}
-	
+
 }
--- src/org/server/MainServer.java
+++ src/org/server/MainServer.java
@@ -1,11 +1,13 @@
 package org.server;
-import org.client.GuiUtil;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.InputStreamReader;
 import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
@@ -13,126 +15,125 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 import java.util.concurrent.CancellationException;
 
-
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-
-
-import java.io.File;
+import org.client.GuiUtil;
 
 public class MainServer {
+
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
-		ServerSocket serverSocket = null;		
+		ServerSocket serverSocket = null;
 
-		//String serverAddress = GuiUtil.getIPAdress();
-		//int port = GuiUtil.getPort();
+		// String serverAddress = GuiUtil.getIPAdress();
+		// int port = GuiUtil.getPort();
 
-		InetAddress IPAddress = InetAddress.getByName(/*serverAddress*/"192.168.0.100");
+		InetAddress IPAddress = InetAddress.getByName(/* serverAddress */"127.0.0.1");
 		serverSocket = new ServerSocket();
 		serverSocket.setReuseAddress(true);
-		serverSocket.bind(new InetSocketAddress(IPAddress, /*port*/5000));
-		
+		serverSocket.bind(new InetSocketAddress(IPAddress, /* port */5000));
+
 		try {
-			while (true) {	
+			while (true) {
 				new Client(serverSocket.accept()).start();
 			}
-		}
-		finally {
-			serverSocket.close();			
+		} finally {
+			serverSocket.close();
 		}
 	}
-	
+
 	// Creates a new thread for each client
 	private static class Client extends Thread {
+		private ObjectOutputStream objectOutput;
 		private Socket socket;
 		private Path rootPath;
 		private Path userPath;
 		private volatile boolean running = true;
-		
+
 		public Client(Socket socket) {
-		    this.socket = socket;
+			this.socket = socket;
+			try {
+				objectOutput = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+				objectOutput.flush();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		
+
 		public void run() {
 			try {
-				
+
 				initializePath();
-			    while (running) {
-			    	String action = getAction();
-			    	if(action != null)
-			    	{
-					    System.out.println(GuiUtil.getOut(socket.getInetAddress().toString(), socket.getPort(), action));
-					    doAction(action);
-			    	}
-			    }
-			}
-            finally{
-                try {
-					socket.close();					
+				while (running) {
+					String action = getAction();
+					if (action != null) {
+						System.out
+								.println(GuiUtil.getOut(socket.getInetAddress().toString(), socket.getPort(), action));
+						doAction(action);
+					}
+				}
+			} finally {
+				try {
+					socket.close();
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-            }
+			}
 		}
-		
+
 		private void initializePath() throws CancellationException {
 			// create the root from which the client cannot escape (server space)
-			String userName = System.getProperty("user.name");			
-			rootPath = Paths.get("C:","Users",userName,"Documents"); // TODO remplacer pour que ca marche dans le laboratoire
-			if(!Files.exists(Paths.get(rootPath.toString(),"root")))
-				new File(rootPath.toString(),"root").mkdir();
-			rootPath = Paths.get(rootPath.toString(),"root");
+			String userName = System.getProperty("user.name");
+			rootPath = Paths.get("C:", "Users", userName, "Documents"); // TODO remplacer pour que ca marche dans le
+																		// laboratoire
+			if (!Files.exists(Paths.get(rootPath.toString(), "root")))
+				new File(rootPath.toString(), "root").mkdir();
+			rootPath = Paths.get(rootPath.toString(), "root");
 			userPath = rootPath;
 		}
-	    
+
 		// Read the action required by the client
 		private String getAction() throws CancellationException {
 			try {
 				String action = null;
-				ObjectInputStream in = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
-				action = (String) in.readObject();							
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//				ObjectInputStream in = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
+				action = (String) in.readLine();
+
+				System.out.println(action);
 				return action;
-			}
-			catch (IOException e) {
+			} catch (IOException e) {
 				e.printStackTrace();
 				try {
 					running = false;
 					socket.close();
-				}catch (IOException f) {
+				} catch (IOException f) {
 					// TODO Auto-generated catch block
 					f.printStackTrace();
 				}
 				return "";
 			}
-			catch (ClassNotFoundException a) {
-				running = false;
-				return "";
-			}
 		}
-		
+
 		// Do the action
 		private void doAction(String action) throws CancellationException {
 			String command = action;
 			String name = "";
-			if(action.contains(" "))
-			{
+			if (action.contains(" ")) {
 				String[] commandAndName = action.split(" ");
 				command = commandAndName[0];
 				name = commandAndName[1];
 			}
-			
+
 			List<String> info = new ArrayList<String>();
-			switch(command) {
+			switch (command) {
 			case "cd":
 				cdAction(name);
 				break;
 			case "pwd":
 				Path relativePath = rootPath.relativize(userPath);
-				if(relativePath.toString().equals(""))
+				if (relativePath.toString().equals(""))
 					info.add(userPath.toString().substring(userPath.toString().lastIndexOf('\\') + 1));
 				else
 					info.add(relativePath.toString());
@@ -156,17 +157,15 @@ public class MainServer {
 				sendInfoToClient(info);
 				break;
 			default:
-				break;					
+				break;
 			}
 		}
-		
+
 		private void sendInfoToClient(List<String> info) throws CancellationException {
 			try {
-			ObjectOutputStream objectOutput = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
-			objectOutput.writeObject(info);
-			objectOutput.flush();
-			}
-			catch (IOException e) {
+				objectOutput.writeObject(info);
+				objectOutput.flush();
+			} catch (IOException e) {
 				try {
 					running = false;
 					socket.close();
@@ -176,129 +175,102 @@ public class MainServer {
 				}
 			}
 		}
-		
+
 		private void cdAction(String name) throws CancellationException {
 			List<String> info = new ArrayList<String>();
 			Path beforeCommandPath = userPath;
 			try {
-				if(name.contains("\\"))
-				{					
-					for (String splitName: name.split("\\\\",0))
-					{
-						if(splitName.equals("."))
-						{
-							if(Files.exists(Paths.get(userPath.toString())))
-								userPath = Paths.get(userPath.toString());					
-							else
-							{
+				if (name.contains("\\")) {
+					for (String splitName : name.split("\\\\", 0)) {
+						if (splitName.equals(".")) {
+							if (Files.exists(Paths.get(userPath.toString())))
+								userPath = Paths.get(userPath.toString());
+							else {
 								info.add("Le dossier n'existe pas\n");
 								userPath = beforeCommandPath;
 								break;
 							}
-						}
-						else if(splitName.equals(".."))
-						{
-							if(Files.exists(Paths.get(userPath.getParent().toString())))
-							{
-								if(userPath.getParent().compareTo(rootPath) >= 0)
+						} else if (splitName.equals("..")) {
+							if (Files.exists(Paths.get(userPath.getParent().toString()))) {
+								if (userPath.getParent().compareTo(rootPath) >= 0)
 									userPath = Paths.get(userPath.getParent().toString());
-								else
-								{
+								else {
+									info.add("Le dossier n'existe pas\n");
+									userPath = beforeCommandPath;
+									break;
+								}
+							}
+						} else {
+
+							if (Files.exists(Paths.get(userPath.toString(), splitName))) {
+								if (userPath.compareTo(rootPath) >= 0)
+									userPath = Paths.get(userPath.toString(), splitName);
+								else {
 									info.add("Le dossier n'existe pas\n");
 									userPath = beforeCommandPath;
 									break;
 								}
 							}
 						}
-						else
-						{
-							
-							if(Files.exists(Paths.get(userPath.toString(),splitName)))
-							{
-								if(userPath.compareTo(rootPath) >= 0)
-									userPath = Paths.get(userPath.toString(),splitName);
-								else
-								{
-									info.add("Le dossier n'existe pas\n");
-									userPath = beforeCommandPath;
-									break;
-								}
-							}
-						}
-					}			
-				}
-				else
-				{
-					if(name.equals("."))
-					{
-						if(Files.exists(Paths.get(userPath.toString())))
-							userPath = Paths.get(userPath.toString());					
-						else
-						{
+					}
+				} else {
+					if (name.equals(".")) {
+						if (Files.exists(Paths.get(userPath.toString())))
+							userPath = Paths.get(userPath.toString());
+						else {
 							info.add("Le dossier n'existe pas\n");
 							userPath = beforeCommandPath;
 						}
-					}
-					else if(name.equals(".."))
-					{
-						if(Files.exists(Paths.get(userPath.getParent().toString())))
-						{
-							if(userPath.getParent().compareTo(rootPath) >= 0)
+					} else if (name.equals("..")) {
+						if (Files.exists(Paths.get(userPath.getParent().toString()))) {
+							if (userPath.getParent().compareTo(rootPath) >= 0)
 								userPath = Paths.get(userPath.getParent().toString());
-							else
-							{
+							else {
 								info.add("Le dossier n'existe pas\n");
 								userPath = beforeCommandPath;
 							}
 						}
-					}
-					else
-					{
-						
-						if(Files.exists(Paths.get(userPath.toString(),name)))
-						{
-							if(userPath.compareTo(rootPath) >= 0)
-								userPath = Paths.get(userPath.toString(),name);
-							else
-							{
+					} else {
+
+						if (Files.exists(Paths.get(userPath.toString(), name))) {
+							if (userPath.compareTo(rootPath) >= 0)
+								userPath = Paths.get(userPath.toString(), name);
+							else {
 								info.add("Le dossier n'existe pas\n");
 								userPath = beforeCommandPath;
 							}
 						}
 					}
 				}
-			}
-			catch(Exception e)
-			{
+			} catch (Exception e) {
 				info.add("Le dossier n'existe pas\n");
 				userPath = beforeCommandPath;
 			}
-								
+
 			Path relativePath = rootPath.relativize(userPath);
-			if(relativePath.toString().equals(""))
-				info.add("Vous êtes dans le dossier : " + userPath.toString().substring(userPath.toString().lastIndexOf('\\') + 1) + "\n");
+			if (relativePath.toString().equals(""))
+				info.add("Vous êtes dans le dossier : "
+						+ userPath.toString().substring(userPath.toString().lastIndexOf('\\') + 1) + "\n");
 			else
 				info.add("Vous êtes dans le dossier : " + relativePath.toString() + "\n");
-			
+
 			sendInfoToClient(info);
 		}
-		
+
 		private void lsAction() throws CancellationException {
-			List<String> info = new ArrayList<String>();			
+			List<String> info = new ArrayList<String>();
 			String[] lsArray = new File(userPath.toString()).list();
-			
-			for(int i= 0; i < lsArray.length; i++)
-			{
-				if(new File(userPath.toString(),lsArray[i]).isDirectory())
+
+			for (int i = 0; i < lsArray.length; i++) {
+				if (new File(userPath.toString(), lsArray[i]).isDirectory())
 					info.add("[Folder]" + lsArray[i]);
 			}
-			
-			for(int i= 0; i < lsArray.length; i++)
-			{
-				if(new File(userPath.toString(),lsArray[i]).isFile())
+
+			for (int i = 0; i < lsArray.length; i++) {
+				if (new File(userPath.toString(), lsArray[i]).isFile())
 					info.add("[File]" + lsArray[i]);
 			}
-			
+
 			sendInfoToClient(info);
 		}
 	}
